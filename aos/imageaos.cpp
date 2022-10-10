303aed21a41949be308ad654371bde4e4ff13e86
asierjustoturuelo <100451012@alumnos.uc3m.es>
2022-10-10 12:26:24 +0200

    arreglado un fallo en del commit anterior

--- aos/imageaos.cpp
+++ aos/imageaos.cpp
@@ -9,7 +9,8 @@ Imageaos::Imageaos(int &arrayR, int &arrayG, int &arrayB) {
     /* Constructor, encargado de asigar a cada nodo de arrayPixeles
      * su valor de R, G, B*/
 
-    for (int i=0; i<sizeof(arrayR); ++i){
+    // El bucle debe recorrer el tamaño de los vectores introducidos
+    for (int i=0; i<sizeof(arrayR)/sizeof(int); ++i){
         this->arrayPixeles[i].R = arrayR[i];
         this->arrayPixeles[i].G = arrayG[i];
         this->arrayPixeles[i].B = arrayB[i];
